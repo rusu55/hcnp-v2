5821b743f00568e628d3e157b62fc1e442bf18df
Bogdan Rusu <rusu55@yahoo.com>
2023-03-08 13:41:48 -0600

    Mobile Menu

--- src/data/hero.js
+++ src/data/hero.js
@@ -1,5 +1,4 @@
-export const carousel = [
-    {image: '/assets/images/electrician2.jpg', title : 'Your LOcal Neighborhood Electrician', text:'Reliable and There When you Need Us'},
+export const carousel = [    
     {image: 'http://picsum.photos/id/1033/900/400', title : 'Slide 2', text:'asta e textul 2'},
     {image: 'http://picsum.photos/id/1037/900/400', title : 'Slide 3', text:'asta e textul 3'},
     {image: 'http://picsum.photos/id/1035/900/400', title : 'Slide 4', text:'asta e textul 4'},
